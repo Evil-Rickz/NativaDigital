ea711fcceb877209ee2ba38c3bbc3451298da980
Rick Lima <ricklimsfs@hotmail.com>
2022-10-27 02:02:46 -0300

    last chages to night

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ SR.reveal(`.about-media`, {origin: 'left', delay: 1800})
 SR.reveal(`.about-data`, {origin: 'right', delay: 1800})
 
 /*----------------------- SLIDE -----------------------*/
-// SR.reveal(`#slides`, {distance: '0px'})
+SR.reveal(`#slides`, {distance: '0px'})
 
 /*----------------------- CUSTOMERS -----------------------*/
 SR.reveal(`.customers-header`)
@@ -35,7 +35,7 @@ SR.reveal(`.contact-content p`, {distance: '0px'})
 SR.reveal(`.contact-buttons`, {origin: 'bottom', delay: 2000})
 
 /*----------------------- FOOTER -----------------------*/
-// SR.reveal(`#footer`, {origin: 'bottom', delay: 1600})
+SR.reveal(`#footer`, {origin: 'bottom', delay: 1600})
 
 
 
