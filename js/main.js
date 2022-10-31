e4d41c867884d39cc11249b0beaf7c977cfec775
Rick Lima <ricklimsfs@hotmail.com>
2022-10-31 14:55:42 -0300

    Scroll Reveal only in home section

--- js/main.js
+++ js/main.js
@@ -16,26 +16,26 @@ SR.reveal(`.impact-phrase2`, {origin: 'right', delay: 4400})
 
 /*----------------------- SERVICES -----------------------*/
 SR.reveal(`.services-header`)
-SR.reveal(`.services-content`, {distance: '0px'})
+// SR.reveal(`.services-content`, {distance: '0px'})
 
-/*----------------------- ABOUT -----------------------*/
-SR.reveal(`.about-title`, {distance: '0px'})
-SR.reveal(`.about-media`, {origin: 'left', delay: 1800})
-SR.reveal(`.about-data`, {origin: 'right', delay: 1800})
+// /*----------------------- ABOUT -----------------------*/
+// SR.reveal(`.about-title`, {distance: '0px'})
+// SR.reveal(`.about-media`, {origin: 'left', delay: 1800})
+// SR.reveal(`.about-data`, {origin: 'right', delay: 1800})
 
-/*----------------------- SLIDE -----------------------*/
-SR.reveal(`#slides`, {distance: '0px'})
+// /*----------------------- SLIDE -----------------------*/
+// SR.reveal(`#slides`, {distance: '0px'})
 
-/*----------------------- CUSTOMERS -----------------------*/
-SR.reveal(`.customers-header`)
-SR.reveal(`.customers-content`, {distance: '0px', delay: 1600})
+// /*----------------------- CUSTOMERS -----------------------*/
+// SR.reveal(`.customers-header`)
+// SR.reveal(`.customers-content`, {distance: '0px', delay: 1600})
 
-/*----------------------- CONTACT -----------------------*/
-SR.reveal(`.contact-content p`, {distance: '0px'})
-SR.reveal(`.contact-buttons`, {origin: 'bottom', delay: 2000})
+// /*----------------------- CONTACT -----------------------*/
+// SR.reveal(`.contact-content p`, {distance: '0px'})
+// SR.reveal(`.contact-buttons`, {origin: 'bottom', delay: 2000})
 
-/*----------------------- FOOTER -----------------------*/
-SR.reveal(`#footer`, {origin: 'bottom', delay: 1600})
+// /*----------------------- FOOTER -----------------------*/
+// SR.reveal(`#footer`, {origin: 'bottom', delay: 1600})
 
 
 
